ad8f05a4778fd8d4e8f6f1076bbb70c7a5561bd1
edrian090 <162600901+edrian090@users.noreply.github.com>
2025-05-23 06:16:22 +0800

    Add Render domain to ALLOWED_HOSTS

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -13,7 +13,7 @@ SECRET_KEY = 'django-insecure--^7367!^s4@felvr(ul_mpmmsjat441s9gh-v8z#csp=!ja9k_
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['clickhaven-django-render.onrender.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
